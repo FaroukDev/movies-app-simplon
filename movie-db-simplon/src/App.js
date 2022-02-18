c7e4c81cd1a84fd1bed91bdbbf7d6ea1c9240f4d
FaroukDev <fa.elayache1@gmail.com>
2022-02-18 14:45:22 +0100

    creating .env file

--- movie-db-simplon/src/App.js
+++ movie-db-simplon/src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from './components/Movie'
 
 
-const FEATURE = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=e59edc16a39691eaa185d6b7049ca9d4"
+const FEATURE_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_API_KEY}`;
 
 
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
 
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=e59edc16a39691eaa185d6b7049ca9d4&query=";
 
 function App() {
 
-  const movies = ['1','2','3']
+  const [ movies, setMovies ] = useState([])
   return (
     <div className="App">
       <header >
        {movies.map(movie =>(
          <Movie/>
-       ))}
+       ))} 
         
       </header>
     </div>
