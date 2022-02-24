7c8ae57b97acb3005d3cd0e4a050331e836417c9
FaroukDev <fa.elayache1@gmail.com>
2022-02-24 16:47:01 +0100

    creating handleOnSubmit and handleOnChange functions to search film in searchBar.js

--- movie-db-simplon/src/App.js
+++ movie-db-simplon/src/App.js
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import Movie from './components/Movie';
-import SearchBar  from "./components/SearchBar";
-
-
+import axios from "axios";
+import Movie from "./components/Movie";
+import SearchBar from "./components/SearchBar";
 
 const FEATURE_API = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_API_KEY}`;
 
-
-
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}query=`;
-
 function App() {
-
-  const [ movies, setMovies ] = useState([]);
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -23,28 +16,22 @@ function App() {
     }
 
     fetchData();
-  }, [FEATURE_API]);
+  }, []);
 
   console.log(movies);
 
   return (
     <div>
-    <SearchBar/>
-    <div className="movie-container">
-      <div>
-        
-      </div >
-      {movies?.map((movie) => (
-      <div>
-        <Movie key={movie.id} {...movie}/>
+      <SearchBar />
+      <div className="movie-container">
+        {movies?.map((movie) => (
+          <div key={movie.id}>
+            <Movie  {...movie} />
+          </div>
+        ))}
       </div>
-      
-      ))}
-      
     </div>
-    </div>
-  )
-  
+  );
 }
 
 export default App;
--- movie-db-simplon/src/components/SearchBar.js
+++ movie-db-simplon/src/components/SearchBar.js
@@ -1,15 +1,51 @@
-import React from 'react';
-import '../css/SearchBar.css'
+import React, { useState, useEffect } from "react";
+import Movie from "./Movie";
+import "../css/SearchBar.css";
+
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=`;
 
 function SearchBar() {
+  const [moviesSearch, setMoviesSearch] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        setMoviesSearch(data.results);
+      });
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+    console.log(setSearchTerm(e.target.value));
+  };
+
   return (
     <div>
-        <header className='header'>
-            <span >The Movie DB</span>
-            <input className='search' type="text" placeholder="Search..." />
-        </header>
+      <header className="header">
+        <span>The Movie DB</span>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {moviesSearch?.map((movie) => (
+          <div>
+            <Movie key={movie.id} {...movie} />
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
