9af73c90a6931cb3e9f187380e24f19011cc5796
FaroukDev <fa.elayache1@gmail.com>
2022-02-25 14:21:44 +0100

    adding style for vote_average in movie.js

--- movie-db-simplon/src/components/Movie.js
+++ movie-db-simplon/src/components/Movie.js
@@ -3,13 +3,26 @@ import "../css/Movie.css";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+  if(vote >= 8 ) {
+    return 'green'
+  } else if (vote >= 6) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
 const Movie = ({ title, poster_path, overview, vote_average }) => {
   return (
     <div className="movie">
-      <img src={IMG_API + poster_path} alt={title} />
+      <img src={poster_path ? (IMG_API + poster_path) : 'https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'} alt={title} />
       <div className="movie-info">
         <h3>{title}</h3>
-        <span>{vote_average}</span>
+        <span className={
+            `tag ${setVoteClass(vote_average)}`}>
+            {vote_average}
+          </span>
       </div>
       <div className="movie-over">
         <h2>Overview</h2>
